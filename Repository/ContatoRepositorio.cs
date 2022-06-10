df6e300a33d899c3df0d742061bee0c93da1ea93
Daniel Guerra <danielespindolaguerr@hotmail.com>
2022-06-10 13:29:42 -0300

    First Commit

---
+++ Repository/ContatoRepositorio.cs
@@ -0,0 +1,31 @@
+﻿using Site.Data;
+using Site.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Site.Repository
+{
+    public class ContatoRepositorio : IContatoRepositorio
+    {
+        private readonly BancoContext _bancoContext;
+        public ContatoRepositorio(BancoContext bancoContext)
+        {
+            _bancoContext = bancoContext;
+        }
+        public List<ContatoModel> BuscarContatos()
+        {
+            return _bancoContext.Contatos.ToList();
+        }
+        public ContatoModel Adicionar(ContatoModel contato)
+        {
+            //Gravar no Banco de Dados
+            _bancoContext.Contatos.Add(contato);
+            _bancoContext.SaveChanges();
+            return contato;
+        }
+
+
+    }
+}
---
+++ Repository/IContatoRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using Site.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Site.Repository
+{
+    public interface IContatoRepositorio
+    {
+        List<ContatoModel> BuscarContatos();
+        ContatoModel Adicionar(ContatoModel contato);
+    }
+}
